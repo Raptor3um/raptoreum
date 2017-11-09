7cc0f0646365a75f924b6fa975973e70774c7de3
Wladimir J. van der Laan <laanwj@gmail.com>
2017-11-09 13:38:33 +0100

    Merge #10696: Remove redundant nullptr checks before deallocation
    b109a1c Remove redundant nullptr checks before deallocation (practicalswift)
    
    Pull request description:
    
      Rationale:
      * `delete ptr` is a no-op if `ptr` is `nullptr`
    
    Tree-SHA512: c98ce769125c4912186a8403cc08a59cfba85b7141af645c709b4c4eb90dd9cbdd6ed8076d50099d1e4ec2bf75917d1af6844082ec42bbb4d94d229a710e051c

--- src/net.cpp
+++ src/net.cpp
@@ -3220,8 +3220,7 @@ CNode::~CNode()
 {
     CloseSocket(hSocket);
 
-    if (pfilter)
-        delete pfilter;
+    delete pfilter;
 }
 
 void CNode::AskFor(const CInv& inv, int64_t doubleRequestDelay)
