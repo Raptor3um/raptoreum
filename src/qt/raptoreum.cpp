4c57f5919c2d77373529c2bc0ac3fdee5260c02c
Wladimir J. van der Laan <laanwj@gmail.com>
2018-01-30 12:34:11 +0100

    Merge #12266: Move scheduler/threadGroup into common-init instead of per-app
    082a61c Move scheduler/threadGroup into common-init instead of per-app (Matt Corallo)
    
    Pull request description:
    
      This resolves #12229 which pointed out a shutdown deadlock due to
      scheduler/checkqueue having been shut down while network message
      processing is still running.
    
    Tree-SHA512: 0c0a76113996b164b0610d3b8c40b396f3e384d165bf098768e31fe3701b00763d0d810ef24702387e2e936fefb9fb900a6225f7417bb0175b585f365d542660

--- src/qt/raptoreum.cpp
+++ src/qt/raptoreum.cpp
@@ -31,7 +31,6 @@
 
 #include <init.h>
 #include <rpc/server.h>
-#include <scheduler.h>
 #include <stacktraces.h>
 #include <ui_interface.h>
 #include <util.h>
@@ -198,8 +197,6 @@ Q_SIGNALS:
     void runawayException(const QString &message);
 
 private:
-    boost::thread_group threadGroup;
-    CScheduler scheduler;
 
     /// Pass fatal exception message to UI thread
     void handleRunawayException(const std::exception_ptr e);
@@ -306,7 +303,7 @@ void BitcoinCore::initialize()
     try
     {
         qDebug() << __func__ << ": Running initialization in thread";
-        bool rv = AppInitMain(threadGroup, scheduler);
+        bool rv = AppInitMain();
         Q_EMIT initializeResult(rv);
     } catch (...) {
         handleRunawayException(std::current_exception());
@@ -322,8 +319,7 @@ void BitcoinCore::restart(QStringList args)
         try
         {
             qDebug() << __func__ << ": Running Restart in thread";
-            Interrupt(threadGroup);
-            threadGroup.join_all();
+            Interrupt();
             StartRestart();
             PrepareShutdown();
             qDebug() << __func__ << ": Shutdown finished";
@@ -343,8 +339,7 @@ void BitcoinCore::shutdown()
     try
     {
         qDebug() << __func__ << ": Running Shutdown in thread";
-        Interrupt(threadGroup);
-        threadGroup.join_all();
+        Interrupt();
         Shutdown();
         qDebug() << __func__ << ": Shutdown finished";
         Q_EMIT shutdownResult();
