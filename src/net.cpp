6edb019b49c6e12a0b59601dc1cbecaca4772be1
Alexander Block <ablock84@gmail.com>
2020-04-22 16:05:11 +0200

    Revert "Only run DisconnectNodes once per second"
    This reverts commit 30e4afae002d0ba24cc930075252af33e3a7c623.

--- src/net.cpp
+++ src/net.cpp
@@ -1873,9 +1873,9 @@ void CConnman::ThreadSocketHandler()
             ForEachNode(AllNodes, [&](CNode* pnode) {
                 InactivityCheck(pnode);
             });
-            DisconnectNodes();
             nLastCleanupNodes = GetTimeMillis();
         }
+        DisconnectNodes();
         NotifyNumConnectionsChanged();
     }
 }
