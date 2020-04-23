94d8d76815b5f5672638393dce27a6e7c2ab511e
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-04-23 12:47:38 +0300

    Fix autobackup functionality

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -81,8 +81,6 @@ static const int64_t TIMESTAMP_MIN = 0;
 //! if set, all keys will be derived by using BIP39/BIP44
 static const bool DEFAULT_USE_HD_WALLET = false;
 
-bool AutoBackupWallet (CWallet* wallet, const std::string& strWalletFile_, std::string& strBackupWarningRet, std::string& strBackupErrorRet);
-
 class CBlockIndex;
 class CCoinControl;
 class COutput;
@@ -1222,8 +1220,9 @@ public:
      */
     void postInitProcess(CScheduler& scheduler);
 
-    /* Initialize AutoBackup functionality */
+    /* AutoBackup functionality */
     static bool InitAutoBackup();
+    bool AutoBackupWallet(const fs::path& wallet_path, std::string& strBackupWarningRet, std::string& strBackupErrorRet);
 
     bool BackupWallet(const std::string& strDest);
 
