2c98bdc4e1a3c80db50838a0f9a142132d6583ad
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-07-14 15:11:07 +0200

    qt: Make use of GUIUtil themed colors/styles (#3561)
    * qt: Update theme colors in GUIUtil
    
    - Adds ThemedColor::GREEN
    - Changes ThemedColor::NEGATIVE to ThemedColor::RED
    - Color adjustments to align with css colors
    
    * qt: Give links a more eye friendly blue
    
    * qt: Use themed colors in net traffic graph
    
    * qt: Use themed styles/color in overview page
    
    * qt: Use themed stylesheets in Intro
    
    * qt: Modified QR code image painting to match the theme colors

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -253,7 +253,7 @@ void Intro::setStatus(int status, const QString &message, quint64 bytesAvailable
         break;
     case FreespaceChecker::ST_ERROR:
         ui->errorMessage->setText(tr("Error") + ": " + message);
-        ui->errorMessage->setStyleSheet("QLabel { color: #800000 }");
+        ui->errorMessage->setStyleSheet(GUIUtil::getThemedStyleQString(GUIUtil::ThemedStyle::TS_ERROR));
         break;
     }
     /* Indicate number of bytes available */
@@ -265,7 +265,7 @@ void Intro::setStatus(int status, const QString &message, quint64 bytesAvailable
         if(bytesAvailable < requiredSpace * GB_BYTES)
         {
             freeString += " " + tr("(of %1 GB needed)").arg(requiredSpace);
-            ui->freeSpace->setStyleSheet("QLabel { color: #800000 }");
+            ui->freeSpace->setStyleSheet(GUIUtil::getThemedStyleQString(GUIUtil::ThemedStyle::TS_ERROR));
         } else {
             ui->freeSpace->setStyleSheet("");
         }
