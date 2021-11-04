codablock/dash

More of bitcoin#10888

src/net.cpp
@@ -2835,8 +2835,8 @@ bool CConnman::RemoveAddedNode(const std::string& strNode)
 bool CConnman::AddPendingMasternode(const CService& service)
 {
     LOCK(cs_vPendingMasternodes);
-    for(std::vector<CService>::const_iterator it = vPendingMasternodes.begin(); it != vPendingMasternodes.end(); ++it) {
-        if (service == *it)
+    for(const auto& s : vPendingMasternodes) {
+        if (service == s)
             return false;
     }
 

Raptor3um/raptoreum

src/net.cpp
@@ -2816,8 +2816,8 @@ bool CConnman::RemoveAddedNode(const std::string& strNode)
 bool CConnman::AddPendingSmartnode(const CService& service)
 {
     LOCK(cs_vPendingSmartnodes);
-    for(std::vector<CService>::const_iterator it = vPendingSmartnodes.begin(); it != vPendingSmartnodes.end(); ++it) {
-        if (service == *it)
+    for(const auto& s : vPendingSmartnodes) {
+        if (service == s)
             return false;
     }
 