bytzck/BYTZ

Fixes after 10753

src/rpc/governance.cpp
@@ -1098,7 +1098,7 @@ static const CRPCCommand commands[] =
     { "dash",               "getgovernanceinfo",      &getgovernanceinfo,      {} },
     { "dash",               "getsuperblockbudget",    &getsuperblockbudget,    {"index"} },
     { "dash",               "gobject",                &gobject,                {} },
-    { "dash",               "voteraw",                &voteraw,                {} },
+    { "dash",               "voteraw",                &voteraw,                {"tx_hash","tx_index","gov_hash","signal","outcome","time","sig"} },
 
 };
 

src/rpc/misc.cpp
@@ -1307,7 +1307,7 @@ static const CRPCCommand commands[] =
 
     /* Dash features */
     { "dash",               "mnsync",                 &mnsync,                 {} },
-    { "dash",               "spork",                  &spork,                  {"value"} },
+    { "dash",               "spork",                  &spork,                  {"arg0","value"} },
 
     /* Not shown in help */
     { "hidden",             "setmocktime",            &setmocktime,            {"timestamp"}},

Raptor3um/raptoreum

src/rpc/governance.cpp
@@ -1099,7 +1099,7 @@ static const CRPCCommand commands[] =
     { "raptoreum",               "getgovernanceinfo",      &getgovernanceinfo,      {} },
     { "raptoreum",               "getsuperblockbudget",    &getsuperblockbudget,    {"index"} },
     { "raptoreum",               "gobject",                &gobject,                {} },
-    { "raptoreum",               "voteraw",                &voteraw,                {} },
+    { "raptoreum",               "voteraw",                &voteraw,                {"tx_hash","tx_index","gov_hash","signal","outcome","time","sig"} },
 
 };
 

src/rpc/misc.cpp
@@ -1308,7 +1308,7 @@ static const CRPCCommand commands[] =
 
     /* Raptoreum features */
     { "raptoreum",               "mnsync",                 &mnsync,            {} },
-    { "raptoreum",               "spork",                  &spork,             {"value"} },
+    { "raptoreum",               "spork",                  &spork,             {"arg0","value"} },
 
     /* Not shown in help */
     { "hidden",             "setmocktime",            &setmocktime,            {"timestamp"}},