bytzck/BYTZ

Streamline processing of pool state updates (#3458)
Changes in behaviour:
- unlock coins, return keys to keypool and set state POOL_STATE_ERROR on error, reset session on timeout later
- drop POOL_STATE_SUCCESS (wasn't really used)

src/privatesend/privatesend-server.cpp
@@ -858,8 +858,8 @@ void CPrivateSendServer::SetState(PoolState nStateNew)
 {
     if (!fMasternodeMode) return;
 
-    if (nStateNew == POOL_STATE_ERROR || nStateNew == POOL_STATE_SUCCESS) {
-        LogPrint(BCLog::PRIVATESEND, "CPrivateSendServer::SetState -- Can't set state to ERROR or SUCCESS as a Masternode. \n");
+    if (nStateNew == POOL_STATE_ERROR) {
+        LogPrint(BCLog::PRIVATESEND, "CPrivateSendServer::SetState -- Can't set state to ERROR as a Masternode. \n");
         return;
     }
 

Raptor3um/raptoreum

src/privatesend/privatesend-server.cpp
@@ -859,8 +859,8 @@ void CPrivateSendServer::SetState(PoolState nStateNew)
 {
     if (!fSmartnodeMode) return;
 
-    if (nStateNew == POOL_STATE_ERROR || nStateNew == POOL_STATE_SUCCESS) {
-        LogPrint(BCLog::PRIVATESEND, "CPrivateSendServer::SetState -- Can't set state to ERROR or SUCCESS as a Smartnode. \n");
+    if (nStateNew == POOL_STATE_ERROR) {
+        LogPrint(BCLog::PRIVATESEND, "CPrivateSendServer::SetState -- Can't set state to ERROR as a Smartnode. \n");
         return;
     }
 