bytzck/BYTZ

Make pending masternode queue proTxHash based
Instead of CService

src/net.cpp
@@ -2126,20 +2126,20 @@ void CConnman::ThreadOpenMasternodeConnections()
 
         // NOTE: Process only one pending masternode at a time
 
-        CService addr;
+        CDeterministicMNCPtr connectToDmn;
         { // don't hold lock while calling OpenMasternodeConnection as cs_main is locked deep inside
             LOCK2(cs_vNodes, cs_vPendingMasternodes);
 
             if (!vPendingMasternodes.empty()) {
-                auto addr2 = vPendingMasternodes.front();
+                auto dmn = mnList.GetValidMN(vPendingMasternodes.front());
                 vPendingMasternodes.erase(vPendingMasternodes.begin());
-                if (!connectedNodes.count(addr2) && !IsMasternodeOrDisconnectRequested(addr2)) {
-                    addr = addr2;
+                if (dmn && !connectedNodes.count(dmn->pdmnState->addr) && !IsMasternodeOrDisconnectRequested(dmn->pdmnState->addr)) {
+                    connectToDmn = dmn;
                 }
             }
 
-            if (addr == CService()) {
-                std::vector<CService> pending;
+            if (!connectToDmn) {
+                std::vector<CDeterministicMNCPtr> pending;
                 for (const auto& group : masternodeQuorumNodes) {
                     for (const auto& proRegTxHash : group.second) {
                         auto dmn = mnList.GetMN(proRegTxHash);
@@ -2153,24 +2153,24 @@ void CConnman::ThreadOpenMasternodeConnections()
                             if (addrInfo.IsValid() && nANow - addrInfo.nLastTry < 60) {
                                 continue;
                             }
-                            pending.emplace_back(addr2);
+                            pending.emplace_back(dmn);
                         }
                     }
                 }
 
                 if (!pending.empty()) {
-                    addr = pending[GetRandInt(pending.size())];
+                    connectToDmn = pending[GetRandInt(pending.size())];
                 }
             }
         }
 
-        if (addr == CService()) {
+        if (!connectToDmn) {
             continue;
         }
 
-        OpenMasternodeConnection(CAddress(addr, NODE_NETWORK));
+        OpenMasternodeConnection(CAddress(connectToDmn->pdmnState->addr, NODE_NETWORK));
         // should be in the list now if connection was opened
-        ForNode(addr, CConnman::AllNodes, [&](CNode* pnode) {
+        ForNode(connectToDmn->pdmnState->addr, CConnman::AllNodes, [&](CNode* pnode) {
             if (pnode->fDisconnect) {
                 return false;
             }
@@ -2782,15 +2782,14 @@ bool CConnman::RemoveAddedNode(const std::string& strNode)
     return false;
 }
 
-bool CConnman::AddPendingMasternode(const CService& service)
+bool CConnman::AddPendingMasternode(const uint256& proTxHash)
 {
     LOCK(cs_vPendingMasternodes);
-    for(const auto& s : vPendingMasternodes) {
-        if (service == s)
-            return false;
+    if (std::find(vPendingMasternodes.begin(), vPendingMasternodes.end(), proTxHash) != vPendingMasternodes.end()) {
+        return false;
     }
 
-    vPendingMasternodes.push_back(service);
+    vPendingMasternodes.push_back(proTxHash);
     return true;
 }
 

src/net.h
@@ -407,7 +407,7 @@ public:
     bool RemoveAddedNode(const std::string& node);
     std::vector<AddedNodeInfo> GetAddedNodeInfo();
 
-    bool AddPendingMasternode(const CService& addr);
+    bool AddPendingMasternode(const uint256& proTxHash);
     bool AddMasternodeQuorumNodes(Consensus::LLMQType llmqType, const uint256& quorumHash, const std::set<uint256>& proTxHashes);
     bool HasMasternodeQuorumNodes(Consensus::LLMQType llmqType, const uint256& quorumHash);
     std::set<uint256> GetMasternodeQuorums(Consensus::LLMQType llmqType);
@@ -543,7 +543,7 @@ private:
     CCriticalSection cs_vOneShots;
     std::vector<std::string> vAddedNodes GUARDED_BY(cs_vAddedNodes);
     CCriticalSection cs_vAddedNodes;
-    std::vector<CService> vPendingMasternodes;
+    std::vector<uint256> vPendingMasternodes;
     std::map<std::pair<Consensus::LLMQType, uint256>, std::set<uint256>> masternodeQuorumNodes; // protected by cs_vPendingMasternodes
     mutable CCriticalSection cs_vPendingMasternodes;
     std::vector<CNode*> vNodes;

src/privatesend/privatesend-client.cpp
@@ -1081,7 +1081,7 @@ bool CPrivateSendClientSession::JoinExistingQueue(CAmount nBalanceNeedsAnonymize
         nSessionDenom = dsq.nDenom;
         mixingMasternode = dmn;
         pendingDsaRequest = CPendingDsaRequest(dmn->pdmnState->addr, CPrivateSendAccept(nSessionDenom, txMyCollateral));
-        connman.AddPendingMasternode(dmn->pdmnState->addr);
+        connman.AddPendingMasternode(dmn->proTxHash);
         // TODO: add new state POOL_STATE_CONNECTING and bump MIN_PRIVATESEND_PEER_PROTO_VERSION
         SetState(POOL_STATE_QUEUE);
         nTimeLastSuccessfulStep = GetTime();
@@ -1158,7 +1158,7 @@ bool CPrivateSendClientSession::StartNewQueue(CAmount nBalanceNeedsAnonymized, C
         }
 
         mixingMasternode = dmn;
-        connman.AddPendingMasternode(dmn->pdmnState->addr);
+        connman.AddPendingMasternode(dmn->proTxHash);
         pendingDsaRequest = CPendingDsaRequest(dmn->pdmnState->addr, CPrivateSendAccept(nSessionDenom, txMyCollateral));
         // TODO: add new state POOL_STATE_CONNECTING and bump MIN_PRIVATESEND_PEER_PROTO_VERSION
         SetState(POOL_STATE_QUEUE);

Raptor3um/raptoreum

src/net.cpp
@@ -2122,20 +2122,20 @@ void CConnman::ThreadOpenSmartnodeConnections()
 
         // NOTE: Process only one pending smartnode at a time
 
-        CService addr;
-        { // don't hold lock while calling OpenSmartnodeConnection as cs_main is locked deep inside
-            LOCK2(cs_vNodes, cs_vPendingSmartnodes);
+        CDeterministicMNCPtr connectToDmn;
+        { // don't hold lock while calling OpenMasternodeConnection as cs_main is locked deep inside
+            LOCK2(cs_vNodes, cs_vPendingMasternodes);
 
             if (!vPendingSmartnodes.empty()) {
-                auto addr2 = vPendingSmartnodes.front();
+                auto dmn = mnList.GetValidMN(vPendingSmartnodes.front());
                 vPendingSmartnodes.erase(vPendingSmartnodes.begin());
-                if (!connectedNodes.count(addr2) && !IsSmartnodeOrDisconnectRequested(addr2)) {
-                    addr = addr2;
+                if (dmn && !connectedNodes.count(dmn->pdmnState->addr) && !IsSmartnodeOrDisconnectRequested(dmn->pdmnState->addr)) {
+                    connectToDmn = dmn;
                 }
             }
 
-            if (addr == CService()) {
-                std::vector<CService> pending;
+            if (!connectToDmn) {
+                std::vector<CDeterministicMNCPtr> pending;
                 for (const auto& group : masternodeQuorumNodes) {
                     for (const auto& proRegTxHash : group.second) {
                         auto dmn = mnList.GetMN(proRegTxHash);
@@ -2149,24 +2149,24 @@ void CConnman::ThreadOpenSmartnodeConnections()
                             if (addrInfo.IsValid() && nANow - addrInfo.nLastTry < 60) {
                                 continue;
                             }
-                            pending.emplace_back(addr2);
+                            pending.emplace_back(dmn);
                         }
                     }
                 }
 
                 if (!pending.empty()) {
-                    addr = pending[GetRandInt(pending.size())];
+                    connectToDmn = pending[GetRandInt(pending.size())];
                 }
             }
         }
 
-        if (addr == CService()) {
+        if (!connectToDmn) {
             continue;
         }
 
-        OpenSmartnodeConnection(CAddress(addr, NODE_NETWORK));
+        OpenSmartnodeConnection(CAddress(connectToDmn->pdmnState->addr, NODE_NETWORK));
         // should be in the list now if connection was opened
-        ForNode(addr, CConnman::AllNodes, [&](CNode* pnode) {
+        ForNode(connectToDmn->pdmnState->addr, CConnman::AllNodes, [&](CNode* pnode) {
             if (pnode->fDisconnect) {
                 return false;
             }
@@ -2778,15 +2778,14 @@ bool CConnman::RemoveAddedNode(const std::string& strNode)
     return false;
 }
 
-bool CConnman::AddPendingSmartnode(const CService& service)
+bool CConnman::AddPendingSmartnode(const uint256& proTxHash)
 {
     LOCK(cs_vPendingSmartnodes);
-    for(const auto& s : vPendingSmartnodes) {
-        if (service == s)
-            return false;
+    if (std::find(vPendingSmartnodes.begin(), vPendingSmartnodes.end(), proTxHash) != vPendingSmartnodes.end()) {
+        return false;
     }
 
-    vPendingSmartnodes.push_back(service);
+    vPendingSmartnodes.push_back(proTxHash);
     return true;
 }
 

src/net.h
@@ -407,7 +407,7 @@ public:
     bool RemoveAddedNode(const std::string& node);
     std::vector<AddedNodeInfo> GetAddedNodeInfo();
 
-    bool AddPendingSmartnode(const CService& addr);
+    bool AddPendingSmartnode(const uint256& proTxHash);
     bool AddSmartnodeQuorumNodes(Consensus::LLMQType llmqType, const uint256& quorumHash, const std::set<uint256>& proTxHashes);
     bool HasSmartnodeQuorumNodes(Consensus::LLMQType llmqType, const uint256& quorumHash);
     std::set<uint256> GetSmartnodeQuorums(Consensus::LLMQType llmqType);
@@ -543,8 +543,8 @@ private:
     CCriticalSection cs_vOneShots;
     std::vector<std::string> vAddedNodes GUARDED_BY(cs_vAddedNodes);
     CCriticalSection cs_vAddedNodes;
-    std::vector<CService> vPendingSmartnodes;
-    std::map<std::pair<Consensus::LLMQType, uint256>, std::set<uint256>> smartnodeQuorumNodes; // protected by cs_vPendingSmartnodes
+    std::vector<uint256> vPendingSmartnodes;
+    std::map<std::pair<Consensus::LLMQType, uint256>, std::set<uint256>> smartnodeQuorumNodes; // protected by cs_vPendingMasternodes
     mutable CCriticalSection cs_vPendingSmartnodes;
     std::vector<CNode*> vNodes;
     std::list<CNode*> vNodesDisconnected;

src/privatesend/privatesend-client.cpp
@@ -1082,7 +1082,7 @@ bool CPrivateSendClientSession::JoinExistingQueue(CAmount nBalanceNeedsAnonymize
         nSessionDenom = dsq.nDenom;
         mixingSmartnode = dmn;
         pendingDsaRequest = CPendingDsaRequest(dmn->pdmnState->addr, CPrivateSendAccept(nSessionDenom, txMyCollateral));
-        connman.AddPendingSmartnode(dmn->pdmnState->addr);
+        connman.AddPendingSmartnode(dmn->proTxHash);
         // TODO: add new state POOL_STATE_CONNECTING and bump MIN_PRIVATESEND_PEER_PROTO_VERSION
         SetState(POOL_STATE_QUEUE);
         nTimeLastSuccessfulStep = GetTime();
@@ -1158,8 +1158,8 @@ bool CPrivateSendClientSession::StartNewQueue(CAmount nBalanceNeedsAnonymized, C
             }
         }
 
-        mixingSmartnode = dmn;
-        connman.AddPendingSmartnode(dmn->pdmnState->addr);
+        mixingMasternode = dmn;
+        connman.AddPendingSmartnode(dmn->proTxHash);
         pendingDsaRequest = CPendingDsaRequest(dmn->pdmnState->addr, CPrivateSendAccept(nSessionDenom, txMyCollateral));
         // TODO: add new state POOL_STATE_CONNECTING and bump MIN_PRIVATESEND_PEER_PROTO_VERSION
         SetState(POOL_STATE_QUEUE);