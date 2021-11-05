bytzck/BYTZ

qt: General qt/c++ related fixes and updates (#3562)
* qt: Draw a border around net traffic graph

* qt: ReceiveRequestDialog - Improve QR code image

- Fix issue with bluriness
- Refine sizing/layout of QR code and address
- Adjust coloring to match the themes

* qt: Give the TransactionView's instantsendWidget a name

Required to access it in css

* qt: Rename conflicting label in SendCoinsDialog

- Was named the same as the the label in EditAddressDialog so it couldn't be accessed properly in css

* qt: Give the TransactionView's search field the first focus on startup

* qt: Some updates to the PrivateSend widget on the OverviewPage

- Hide denom labels if inactive
- Enable wordwrap for denom label
- Add some spacer
- Make sure it gets its basic initialization on startup

* qt: Fix some layout issues in SendCoinsDialog's UI file.

- Added some margins for fee selection radio buttons to align them centered to their options
- Removed a weird placed spacer

* qt: Fix vertical alignment of the two balance labels in SendCoinsDialog

* qt: Add newline in textedit of receiverequest

* qt: OptionsDialog - Hide override hint if there is nothing overridden

* qt: Allow stylesheet modifications for auto completition popup

- Inheritance doesn't work here obviously because of QCompleter is parent of the popup
- QStyledItemDelegate delegate is required. Without its not possible to access `::item` selectors from css.

* qt: Make the progress label in the status bar accessible in css

* qt: Update weekend colors of QCalendarWidget on style changes

Its obviously not possible to do this in stylesheets thats why i added
this as workaround.

* qt: Load stylesheets for Intro

This is the datadir selection dialog.

* Drop labelPrivateSendLastMessage

* Add a space

Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>

Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
Co-authored-by: Pa

src/qt/overviewpage.cpp
@@ -305,6 +305,9 @@ void OverviewPage::setWalletModel(WalletModel *model)
         // explicitly update PS frame and transaction list to reflect actual settings
         updateAdvancedPSUI(model->getOptionsModel()->getShowAdvancedPSUI());
 
+        // Initialize PS UI
+        privateSendStatus(true);
+
         if(!privateSendClient.fEnablePrivateSend) return;
 
         connect(model->getOptionsModel(), SIGNAL(privateSendRoundsChanged()), this, SLOT(updatePrivateSendProgress()));
@@ -467,14 +470,22 @@ void OverviewPage::updateAdvancedPSUI(bool fShowAdvancedPSUI) {
     ui->privateSendProgress->setVisible(fShowAdvancedPSUI);
     ui->labelSubmittedDenomText->setVisible(fShowAdvancedPSUI);
     ui->labelSubmittedDenom->setVisible(fShowAdvancedPSUI);
-    ui->labelPrivateSendLastMessage->setVisible(fShowAdvancedPSUI);
 }
 
-void OverviewPage::privateSendStatus()
+void OverviewPage::privateSendStatus(bool fForce)
 {
-    if(!masternodeSync.IsBlockchainSynced() || ShutdownRequested()) return;
+    if (!fForce && (!masternodeSync.IsBlockchainSynced() || ShutdownRequested())) return;
 
     if(!walletModel) return;
+
+    auto tempWidgets = {ui->labelSubmittedDenomText,
+                        ui->labelSubmittedDenom};
+
+    auto setWidgetsVisible = [&](bool fVisible) {
+        for (const auto& it : tempWidgets) {
+            it->setVisible(fVisible);
+        }
+    };
 
     static int64_t nLastDSProgressBlockTime = 0;
     int nBestHeight = clientModel->getNumBlocks();
@@ -495,7 +506,7 @@ void OverviewPage::privateSendStatus()
             updatePrivateSendProgress();
         }
 
-        ui->labelPrivateSendLastMessage->setText("");
+        setWidgetsVisible(false);
         ui->togglePrivateSend->setText(tr("Start Mixing"));
 
         QString strEnabled = tr("Disabled");
@@ -574,14 +585,7 @@ void OverviewPage::privateSendStatus()
         updatePrivateSendProgress();
     }
 
-    QString strStatus = QString(privateSendClient.GetStatuses().c_str());
-
-    QString s = tr("Last PrivateSend message:\n") + strStatus;
-
-    if(s != ui->labelPrivateSendLastMessage->text())
-        LogPrint(BCLog::PRIVATESEND, "OverviewPage::privateSendStatus -- Last PrivateSend message: %s\n", strStatus.toStdString());
-
-    ui->labelPrivateSendLastMessage->setText(s);
+    setWidgetsVisible(true);
 
     ui->labelSubmittedDenom->setText(QString(privateSendClient.GetSessionDenoms().c_str()));
 }

Raptor3um/raptoreum

src/qt/overviewpage.cpp
@@ -306,6 +306,9 @@ void OverviewPage::setWalletModel(WalletModel *model)
         // explicitly update PS frame and transaction list to reflect actual settings
         updateAdvancedPSUI(model->getOptionsModel()->getShowAdvancedPSUI());
 
+        // Initialize PS UI
+        privateSendStatus(true);
+
         if(!privateSendClient.fEnablePrivateSend) return;
 
         connect(model->getOptionsModel(), SIGNAL(privateSendRoundsChanged()), this, SLOT(updatePrivateSendProgress()));
@@ -468,14 +471,22 @@ void OverviewPage::updateAdvancedPSUI(bool fShowAdvancedPSUI) {
     ui->privateSendProgress->setVisible(fShowAdvancedPSUI);
     ui->labelSubmittedDenomText->setVisible(fShowAdvancedPSUI);
     ui->labelSubmittedDenom->setVisible(fShowAdvancedPSUI);
-    ui->labelPrivateSendLastMessage->setVisible(fShowAdvancedPSUI);
 }
 
-void OverviewPage::privateSendStatus()
+void OverviewPage::privateSendStatus(bool fForce)
 {
-    if(!smartnodeSync.IsBlockchainSynced() || ShutdownRequested()) return;
+    if (!fForce && (!smartnodeSync.IsBlockchainSynced() || ShutdownRequested())) return;
 
     if(!walletModel) return;
+
+    auto tempWidgets = {ui->labelSubmittedDenomText,
+                        ui->labelSubmittedDenom};
+
+    auto setWidgetsVisible = [&](bool fVisible) {
+        for (const auto& it : tempWidgets) {
+            it->setVisible(fVisible);
+        }
+    };
 
     static int64_t nLastDSProgressBlockTime = 0;
     int nBestHeight = clientModel->getNumBlocks();
@@ -496,7 +507,7 @@ void OverviewPage::privateSendStatus()
             updatePrivateSendProgress();
         }
 
-        ui->labelPrivateSendLastMessage->setText("");
+        setWidgetsVisible(false);
         ui->togglePrivateSend->setText(tr("Start Mixing"));
 
         QString strEnabled = tr("Disabled");
@@ -575,14 +586,7 @@ void OverviewPage::privateSendStatus()
         updatePrivateSendProgress();
     }
 
-    QString strStatus = QString(privateSendClient.GetStatuses().c_str());
-
-    QString s = tr("Last PrivateSend message:\n") + strStatus;
-
-    if(s != ui->labelPrivateSendLastMessage->text())
-        LogPrint(BCLog::PRIVATESEND, "OverviewPage::privateSendStatus -- Last PrivateSend message: %s\n", strStatus.toStdString());
-
-    ui->labelPrivateSendLastMessage->setText(s);
+    setWidgetsVisible(true);
 
     ui->labelSubmittedDenom->setText(QString(privateSendClient.GetSessionDenoms().c_str()));
 }