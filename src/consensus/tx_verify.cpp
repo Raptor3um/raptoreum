df7f6bdaaa4b35254f46b353f4ba8cc5baefabc6
Fernando Feiden <fernandofeiden@gmail.com>
2022-08-02 23:58:16 -0400

    Update tx_verify.cpp

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -55,8 +55,8 @@ static const char *validateFutureCoin(const Coin& coin, int nSpendHeight) {
 			CFutureTx futureTx;
 			if(GetTxPayload(coin.vExtraPayload, futureTx)) {
                 		bool isBlockMature = futureTx.maturity > 0 && nSpendHeight - coin.nHeight >= futureTx.maturity;
-               			bool isTimeMature = futureTx.lockTime > 0 && adjustCurrentTime - confirmedTime  >= futureTx.lockTime;
-                		bool canSpend = isBlockMature || isTimeMature;
+				bool isTimeMature = futureTx.lockTime > 0 && adjustCurrentTime - confirmedTime  >= futureTx.lockTime;
+				bool canSpend = isBlockMature || isTimeMature;
 				if(!canSpend) {
 					return "bad-txns-premature-spend-of-future";
 				}
