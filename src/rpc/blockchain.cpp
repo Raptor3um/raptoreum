3f08f6afdf2b668fdd61626eda34a638fe2a3761
npq7721 <tri282@gmail.com>
2022-08-17 13:37:42 -0700

    minor UI improvements and bug fixed (#179)
    * update build to build release candidate
    
    * remove ubuntu 22 and put 18 back. 22 not yet build
    
    * correct build version extraction for release candidate build
    
    * add back accidental line removal
    
    * clean up (#177)
    
    Co-authored-by: nandofw <dev.nandofw@gmail.com>
    
    * change v17 deployment time to sep 28 and fork block to 410000
    
    * increase coinjoin amount limit in UI
    
    * fix scale (#178)
    
    removed duplicated call to getScaledFontSize as getFont already call to getScaledFontSize
    
    Co-authored-by: Fernando Feiden <fernandofeiden@gmail.com>
    Co-authored-by: nandofw <dev.nandofw@gmail.com>

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1481,9 +1481,9 @@ UniValue getblockchaininfo(const JSONRPCRequest& request)
     // softforks.push_back(SoftForkDesc("bip34", 2, tip, consensusParams));
     // softforks.push_back(SoftForkDesc("bip66", 3, tip, consensusParams));
     // softforks.push_back(SoftForkDesc("bip65", 4, tip, consensusParams));
-    // for (int pos = Consensus::DEPLOYMENT_CSV; pos != Consensus::MAX_VERSION_BITS_DEPLOYMENTS; ++pos) {
-    //     BIP9SoftForkDescPushBack(bip9_softforks, consensusParams, static_cast<Consensus::DeploymentPos>(pos));
-    // }
+     for (int pos = Consensus::DEPLOYMENT_V17; pos != Consensus::MAX_VERSION_BITS_DEPLOYMENTS; ++pos) {
+         BIP9SoftForkDescPushBack(bip9_softforks, consensusParams, static_cast<Consensus::DeploymentPos>(pos));
+     }
     obj.pushKV("softforks",             softforks);
     obj.pushKV("bip9_softforks", bip9_softforks);
 
