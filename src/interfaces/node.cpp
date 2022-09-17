253101ff86456df33b8d5bbecda1b0ca30e7618c
m@lbit <michal@arqma.com>
2022-09-17 05:00:06 +0100

    missing override

--- src/interfaces/node.cpp
+++ src/interfaces/node.cpp
@@ -268,7 +268,7 @@ public:
         }
         return false;
     }
-    int64_t getTotalBytesRecv()  { return m_context->connman ? m_context->connman->GetTotalBytesRecv() : 0; }
+    int64_t getTotalBytesRecv() override { return m_context->connman ? m_context->connman->GetTotalBytesRecv() : 0; }
     int64_t getTotalBytesSent() override { return m_context->connman ? m_context->connman->GetTotalBytesSent() : 0; }
     size_t getMempoolSize() override { return m_context->mempool ? m_context->mempool->size() : 0; }
     size_t getMempoolDynamicUsage() override { return m_context->mempool ? m_context->mempool->DynamicMemoryUsage() : 0; }
