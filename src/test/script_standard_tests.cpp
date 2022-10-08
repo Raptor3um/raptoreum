df01a7eab9d341a6a3472fe93285d27417c80685
nandofw <fernandofeiden@gmail.com>
2022-10-08 19:38:06 -0300

    script standard tests (#225)
    Co-authored-by: nandofw <dev.nandofw@gmail.com>

--- src/test/script_standard_tests.cpp
+++ src/test/script_standard_tests.cpp
@@ -100,7 +100,7 @@ BOOST_AUTO_TEST_CASE(script_standard_Solver_success)
     // TX_NONSTANDARD
     s.clear();
     s << OP_9 << OP_ADD << OP_11 << OP_EQUAL;
-    BOOST_CHECK(Solver(s, whichType, solutions));
+    BOOST_CHECK(!Solver(s, whichType, solutions));
     BOOST_CHECK_EQUAL(whichType, TX_NONSTANDARD);
 }
 
